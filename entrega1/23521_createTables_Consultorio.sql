47dbb236f7b15a12dcc19ea73c3d5be97fd830ce
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2023-11-17 20:35:16 -0300

    Atualiza esquema e nomes de campo
    - Utiliza esquema padrão.
    - Identificador das PKs das tabelas Tipo_Consulta e Status_Consulta
      atualizadas.

--- entrega1/23521_createTables_Consultorio.sql
+++ entrega1/23521_createTables_Consultorio.sql
@@ -1,52 +1,52 @@
-create schema consultorio
-
-create table consultorio.Tipo_Consulta (
-	codConsulta int identity,
+create table Tipo_Consulta (
+	codTConsulta int identity,
 	descricao text not null,
 	constraint PK_codConsulta primary key (codConsulta)
 )
 
-create table consultorio.Status_Consulta (
-	codStatus int identity,
+create table Status_Consulta (
+	codSConsulta int identity,
 	descricao varchar(25) not null,
 	constraint PK_codStatus primary key (codStatus)
 )
 
-create table consultorio.Especialidade_Medica (
+create table Especialidade_Medica (
 	codEspecialidade int identity,
 	descricao varchar(25) not null,
 	constraint PK_codEspecialidade primary key (codEspecialidade)
 )
 
-create table consultorio.Medico (
+create table Medico (
 	idMedico int identity,
 	codEspecialidade int,
 	prenome varchar(50) not null,
 	sobrenome varchar(50) not null,
 	email varchar(50) not null,
+	senha varchar(16) not null,
 	constraint PK_idMedico primary key (idMedico),
-	constraint FK_codEspecialidade foreign key (codEspecialidade) references consultorio.Especialidade_Medica(codEspecialidade)
+	constraint FK_codEspecialidade foreign key (codEspecialidade) references Especialidade_Medica(codEspecialidade)
 )
 
-create table consultorio.Paciente (
+create table Paciente (
 	idPaciente int identity,
 	prenome varchar(50) not null,
 	sobrenome varchar(50) not null,
 	email varchar(50) not null,
+	senha varchar(16) not null,
 	constraint PK_idPaciente primary key (idPaciente)
 )
 
-create table consultorio.Consulta (
+create table Consulta (
 	idConsulta int identity,
-	codConsulta int,
-	codStatus int,
+	codTConsulta int,
+	codSConsulta int,
 	idMedico int,
 	idPaciente int,
 	dataConsulta date not null,
 	horaConsulta time not null,
 	constraint PK_idConsulta primary key (idConsulta),
-	constraint FK_codConsulta foreign key (codConsulta) references consultorio.Tipo_Consulta(codConsulta),
-	constraint FK_codStatus foreign key (codStatus) references consultorio.Status_Consulta(codStatus),
-	constraint FK_idMedico foreign key (idMedico) references consultorio.Medico(idMedico),
-	constraint FK_idPaciente foreign key (idPaciente) references consultorio.Paciente(idPaciente)
+	constraint FK_codTConsulta foreign key (codTConsulta) references Tipo_Consulta(codTConsulta),
+	constraint FK_codSConsulta foreign key (codSConsulta) references Status_Consulta(codSConsulta),
+	constraint FK_idMedico foreign key (idMedico) references Medico(idMedico),
+	constraint FK_idPaciente foreign key (idPaciente) references Paciente(idPaciente)
 )
